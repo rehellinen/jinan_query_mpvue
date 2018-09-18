116f0b5eb085e7dfc8d0fe236ff829a26914b081
rehellinen <912377791@qq.com>
2018-09-18 12:59:33 +0800

    卡片列表页样式

--- src/pages/edit-card/edit-card.vue
+++ src/pages/edit-card/edit-card.vue
@@ -1,6 +1,17 @@
 <template lang="pug">
 div.container
-  p 123
+  div.single.card
+    img.select(src="__IMAGE__/icon/selected.png")
+    p 电费查询
+    div
+      img(src="__IMAGE__/icon/arrow@up.png")
+      img.down(src="__IMAGE__/icon/arrow@up.png")
+  div.single.card
+    img.select(src="__IMAGE__/icon/noSelected.png")
+    p 课程表查询
+    div
+      img(src="__IMAGE__/icon/arrow@up.png")
+      img.down(src="__IMAGE__/icon/arrow@up.png")
 </template>
 
 <script>
@@ -16,4 +27,26 @@ export default {
 
 <style scoped lang="sass">
   @import "~sass/base"
+
+  .single
+    display: flex
+    height: 100rpx
+    align-items: center
+    img
+      width: 40rpx
+      height: 40rpx
+    .select
+      margin-left: 20rpx
+    p
+      font-size: $normal-font-size
+      margin-left: 40rpx
+      color: $pink
+    div
+      display: flex
+      justify-content: flex-end
+      flex: 1
+      .down
+        transform: rotate(180deg)
+        margin: 0 20rpx
+  .de
 </style>
